94bbb4aaf8d14afa264854d8db399e9991cffd72
Stefan Saraev <stefan@saraev.ca>
2025-08-10 13:14:44 +0300

    trixie: cosmetics, uefi boot

--- trixie/config.sh
+++ trixie/config.sh
@@ -6,16 +6,17 @@ lb config \
   --apt-options "--yes -oAcquire::Languages=none -oAcquire::PDiffs=false -oAPT::Install-Recommends=0 -oAPT::Install-Suggests=0 -oDpkg::Progress-Fancy=true" \
   --apt-recommends false \
   --apt-source-archives false \
-  -a amd64 \
-  -b iso-hybrid \
+  --architecture amd64 \
+  --binary-image iso-hybrid \
   --bootappend-live "boot=live components loglevel=3" \
   --bootappend-live-failsafe "boot=live components memtest noapic noapm nodma nomce nolapic nomodeset nosmp nosplash vga=normal" \
-  --bootloader "syslinux" \
+  --bootloaders "syslinux,grub-efi" \
+  --checksums none \
+  --debian-installer none \
   --debootstrap-options "--variant=minbase" \
-  -d trixie \
+  --distribution trixie \
   --iso-volume "trixie" \
   --archive-areas "main contrib non-free non-free-firmware" \
   --parent-archive-areas "main contrib non-free non-free-firmware" \
   --linux-packages "linux-image linux-headers" \
-  --firmware-binary false \
   --firmware-chroot false
